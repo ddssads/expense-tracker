cd533c822f13cf22c446d9eaae3586ff3576f88f
ddssads <tw0937608360@gmail.com>
2020-12-28 13:04:24 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const { urlencoded } = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 3000
 //set template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
