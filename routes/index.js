f369c6068ca6dc570d307695980429d8f9fa5fb7
ddssads <tw0937608360@gmail.com>
2021-01-18 01:07:34 +0800

    feat: add user modules & login/register page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
+const user = require('./modules/user')
 const expenseTracker = require('./modules/expensetracker')
 
 router.use('/', home)
 router.use('/expense', expenseTracker)
+router.use('/users', user)
 
 module.exports = router
---
+++ routes/modules/user.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', (req, res) => {
+  console.log(req.body)
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  console.log(req.body)
+})
+
+module.exports = router
