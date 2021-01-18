028422f0d626742f118cbb67108366c4e7a7bd99
ddssads <tw0937608360@gmail.com>
2021-01-18 15:06:43 +0800

    feat:add user authenticator

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const user = require('./modules/user')
+const { authenticator } = require('../middleware/auth')
 const expenseTracker = require('./modules/expensetracker')
 
-router.use('/', home)
-router.use('/expense', expenseTracker)
+
+router.use('/expense', authenticator, expenseTracker)
 router.use('/users', user)
+router.use('/', authenticator, home)
 
 module.exports = router
